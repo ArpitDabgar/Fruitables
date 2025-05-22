919d0569995ef5d8094001fc6fa7180525e612ac
arpit201299 <arpitdabgar009@gmail.com>
2025-05-22 16:27:31 +0530

    first commit

--- project/settings.py
+++ project/settings.py
@@ -158,14 +158,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-# Email settings for sending OTPs
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # or your provider
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'arpitdabgar009@gmail.com'
-EMAIL_HOST_PASSWORD = 'vwtvjegrsyqosurr'
-
 
 
 MESSAGE_TAGS={message_constants.DEBUG:'debug'}  
